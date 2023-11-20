37f9c35be009143741906fce1ea84e66d2c64ba0
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2023-11-20 10:16:29 +0530

    changes

--- Neural Network/NeuralNetwork.py
+++ Neural Network/NeuralNetwork.py
@@ -6,13 +6,10 @@ class NeuralNetwork():
     def __init__(self,input_nodes,hidden_nodes1,output_nodes):
         self.input_nodes = input_nodes
         self.hidden_nodes1 = hidden_nodes1
-        # self.hidden_nodes2 = hidden_nodes2
         self.output_nodes = output_nodes
         self.in_hidden1_weights = np.random.rand(self.hidden_nodes1,self.input_nodes)
-        # self.h1_h2_weights = np.random.rand(self.hidden_nodes2,self.hidden_nodes1)
         self.hidden1_output_weights = np.random.rand(self.output_nodes,self.hidden_nodes1)
         self.in_hidden1_biases = np.random.rand(self.hidden_nodes1,1)
-        # self.h1_h2_biases = np.random.rand(self.hidden_nodes2,1)
         self.hidden1_output_biases = np.random.rand(self.output_nodes,1)
         self.sigmoid_v = np.vectorize(self.sigmoid)
 
@@ -24,10 +21,6 @@ class NeuralNetwork():
 
         self.hidden_layer1 = self.in_hidden1_weights.dot(self.inputs)
         self.hidden_layer1=self.sigmoid_v(self.hidden_layer1+self.in_hidden1_biases)
-
-        # self.hidden_layer2 = self.h1_h2_weights.dot(self.hidden_layer1)
-        # self.hidden_layer2 = self.sigmoid_v(self.hidden_layer2+self.h1_h2_biases)
-
         self.output = self.hidden1_output_weights.dot(self.hidden_layer1)
         self.output =self.sigmoid_v(self.output+self.hidden1_output_biases)
         return self.output
